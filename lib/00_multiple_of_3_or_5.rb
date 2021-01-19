b14b1462fc569038e2f4d27b00f143bdf185ba4f
Charles <charlescampet03@gmail.com>
2021-01-19 15:51:43 +0100

    first commit

--- lib/00_multiple_of_3_or_5.rb
+++ lib/00_multiple_of_3_or_5.rb
@@ -1,6 +1,21 @@
 #Loop method
 def sum_of_3_or_5_multiples(final_number)
-    (0..final_number).each
+    final_sum = 0
+
+    integer_y_n = final_number.is_a? Integer
+    if (integer_y_n == true)
+        if (final_number < 0)
+            return "Yo, Je ne prend que les entiers naturels!"
+        end
+        (0..final_number - 1).each do |index|
+            if is_multiple_of_3_or_5?(index)
+                final_sum += index
+            end
+        end
+    else
+        return "Yo, Je ne prend que les entiers naturels!"
+    end
+    return final_sum
 end
 
 #Modulo method
